05b64aaecf11ba2127afe12b0f72a09493286b31
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-05-07 20:14:37 +0300

    Next Solution

---
+++ ShortestWord.py
@@ -0,0 +1,20 @@
+"""Simple, given a string of words, return the length of the shortest word(s).
+   String will never be empty, and you do not need to account for different data types."""
+
+import pytest
+
+
+def find_short(s):
+    """My_Solution"""
+    return min(len(l) for l in s.split())
+
+
+def test_find_short():
+    """My_test_Pytest"""
+    assert find_short("Hello World") == 5
+    assert find_short("Hello my friend") == 2
+    assert find_short("Это тест") == 3
+
+
+if __name__ == "__main__":
+    test_find_short()
