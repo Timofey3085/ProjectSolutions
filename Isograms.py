9414e780ab619a01f00b04d6e928b41ca6cf143b
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-06-13 08:35:53 +0300

    Next Solution

--- Isograms.py
+++ Isograms.py
@@ -2,7 +2,6 @@
 An isogram is a word that has no repeating letters, consecutive or non-consecutive.
 Implement a function that determines whether a string that contains only letters is an isogram.
 Assume the empty string is an isogram. Ignore letter case.
-
 Example: (Input --> Output)
 "Dermatoglyphics" --> true
 "aba" --> false
