682f5ce10cce6fbd2aa08c5a0f1022db07bdfc08
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-06-23 13:07:58 +0300

    Next Solution

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+"""
+Написал простой калькулятор и с помощью сборщика PyInstaller
+упаковал код для возможности запуска без использования IDE.
+"""
+
+
+def calculate(x: int, y: int, operator: str) -> float:
+    if operator == "+":
+        return x + y
+    elif operator == "-":
+        return x - y
+    elif operator == "/":
+        return x / y
+    elif operator == "*":
+        return x * y
+    elif operator == "**":
+        return x ** y
+    else:
+        print("Некорректный оператор. Введите арифметическое действие (+, -, /, *, **): ")
+        new_operator: str = input()
+        return calculate(x, y, new_operator)
+
+
+operator: str = input("Введите арифметическое действие (+, -, /, *, **): ")
+x = int(input("Введите первое число: x = "))
+y = int(input("Введите второе число: y = "))
+
+result = calculate(x, y, operator)
+print("Результат:", round(result))
+
+input("Press Enter to exit...")
