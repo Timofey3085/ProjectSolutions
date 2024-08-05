aba9ad589c62f01a0cc5b0f3b26bafd909835a9a
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-08-05 20:50:42 +0300

    Next Solution

--- Return_the_Missing_Element.py
+++ Return_the_Missing_Element.py
@@ -1,10 +1,8 @@
 """
 Fellow code warrior, we need your help!
 We seem to have lost one of our sequence elements, and we need your help to retrieve it!
-
 Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order),
 but one of them seems to be missing.
-
 Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
 
 Examples:
