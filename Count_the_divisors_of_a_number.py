e507a71bb70d072a5eaa4bd62ef2c13d7378dcd2
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-08-06 21:57:25 +0300

    Next Solution

--- Count_the_divisors_of_a_number.py
+++ Count_the_divisors_of_a_number.py
@@ -1,8 +1,6 @@
 """
 Count the number of divisors of a positive integer n.
-
 Random tests go up to n = 500000.
-
 Examples (input --> output)
 4 --> 3 // we have 3 divisors - 1, 2 and 4
 5 --> 2 // we have 2 divisors - 1 and 5
