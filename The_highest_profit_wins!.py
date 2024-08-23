af95dd49ca04f9cc3d826d565feb5a7e97e083bb
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-08-23 20:39:44 +0300

    Next Solution

--- The_highest_profit_wins!.py
+++ The_highest_profit_wins!.py
@@ -27,6 +27,7 @@ class MinMax(unittest.TestCase):
         self.assertEqual(min_max([1, 2, 3, 4, 5]), [1, 5])
         self.assertEqual(min_max([2334454, 5]), [5, 2334454])
         self.assertEqual(min_max([1]), [1, 1])
+        self.assertEqual(min_max([2]), [2, 2])
 
 
 if __name__ == '__main__':
