714641cd2dc7dd6b33b6098097ffb5c21ddb1e03
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-09-05 21:32:04 +0300

    Next Solution

--- Number_Pairs.py
+++ Number_Pairs.py
@@ -1,8 +1,6 @@
 """
 In this kata the aim is to compare each pair of integers from two arrays, and return a new array of large numbers.
-
 Note: Both arrays have the same dimensions.
-
 Example:
 arr1 = [13, 64, 15, 17, 88]
 arr2 = [23, 14, 53, 17, 80]
