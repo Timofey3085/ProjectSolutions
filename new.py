5ebf923fd0ff808310b57b017cb94b6d179f5736
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-09-30 21:09:47 +0300

    Next Solution

---
+++ new.py
@@ -0,0 +1,17 @@
+"""
+Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n.
+Assume that m is a positive integer.
+Ex.
+multiples(3, 5.0)
+should return
+[5.0, 10.0, 15.0]
+"""
+import pytest
+
+
+def multiples(m, n):
+    """My_solution"""
+    return [n * i for i in range(1, m + 1)]
+
+
+
