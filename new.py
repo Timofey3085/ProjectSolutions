adb0bb869458ad84519d70ceeb7d57532eebc559
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-03 21:24:46 +0300

    Next Solution

---
+++ new.py
@@ -0,0 +1,19 @@
+"""
+Return the number (count) of vowels in the given string.
+We will consider a, e, i o, u y).
+The input string will only consist of lower case letters and/or spaces.
+"""
+import pytest
+
+
+def get_count(sentence):
+    """My_solution"""
+    data = ["a", "e", "i", "o", "u"]
+    count = 0
+    for i in sentence:
+        if i in data:
+            count += 1
+    return count
+
+
+
