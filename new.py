b1b6ed4f0e81ce01770abe5552e07d59bd896368
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-09 22:09:45 +0300

    Next Solution

--- new.py
+++ new.py
@@ -7,3 +7,14 @@ capitalize("abcdef",[1,2,5]) = "aBCdeF"
 capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
 The input will be a lowercase string with no spaces and an array of digits.
 """
+
+
+def capitalize(s, ind):
+    """My_solution"""
+    lst = []
+    for i, k in enumerate(s):
+        if i in ind:
+            lst.append(k.upper())
+        else:
+            lst.append(k)
+    return "".join(lst)
