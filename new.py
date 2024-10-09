3988382765e34db57988849edf8962741605b885
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-09 22:12:40 +0300

    Next Solution

--- new.py
+++ new.py
@@ -7,6 +7,7 @@ capitalize("abcdef",[1,2,5]) = "aBCdeF"
 capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
 The input will be a lowercase string with no spaces and an array of digits.
 """
+import pytest
 
 
 def capitalize(s, ind):
@@ -18,3 +19,16 @@ def capitalize(s, ind):
         else:
             lst.append(k)
     return "".join(lst)
+
+
+@pytest.mark.parametrize("s, ind, expected_result", [
+    ("abcdef",[1,2,5],'aBCdeF'),
+    ("abcdef",[1,2,5,100],'aBCdeF',),
+])
+def test_capitalize(s, ind, expected_result):
+    assert capitalize(s, ind) == expected_result
+
+
+if __name__ == '__main__':
+    pytest.main()
+
