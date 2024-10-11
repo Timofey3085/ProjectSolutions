90527e3c53f1520cfe912f3e20fa26d0342dcb10
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-11 21:13:14 +0300

    Next Solution

--- new.py
+++ new.py
@@ -14,6 +14,7 @@ def no_odds(values):
     ([0, 1], [0]),
     ([0, 1, 2, 3], [0, 2]),
     ([1, 3, 5, 7, 9], []),
+    ([0, 2, 4, 6, 8, 10], [0, 2, 4, 6, 8, 10]),
 ])
 def test_no_odds(values, result):
     assert no_odds(values) == result
