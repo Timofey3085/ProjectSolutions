c183eaa9c93c6e0874f78a8ad179f4cf1e219825
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-18 21:09:29 +0300

    Next Solution

---
+++ new.py
@@ -0,0 +1,11 @@
+"""
+We need a function that receives two arguments, a value val , and the exponent of the power,  pow_ ,
+and outputs the value that we want to find.
+Let'see some cases:
+(12385, 3) ==> 13824
+(1245678, 5) ==> 1419857
+The value, val will be always a positive integer.
+The power, pow_ , always higher than 1 .
+"""
+
+
