c92fd2c71d91a8177067fd5b80b2101dee85f322
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-20 12:11:00 +0300

    Next Solution

--- new.py
+++ new.py
@@ -5,3 +5,41 @@ Input will be a string with length > 0.
 """
 
 
+def move_ten(st):
+    """My_solution"""
+    alphabet = {
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 3,
+        "e": 4,
+        "f": 5,
+        "g": 6,
+        "h": 7,
+        "i": 8,
+        "j": 9,
+        "k": 10,
+        "l": 11,
+        "m": 12,
+        "n": 13,
+        "o": 14,
+        "p": 15,
+        "q": 16,
+        "r": 17,
+        "s": 18,
+        "t": 19,
+        "u": 20,
+        "v": 21,
+        "w": 22,
+        "x": 23,
+        "y": 24,
+        "z": 25,
+    }
+
+    result = ""
+
+    for i in st:
+        if i in alphabet:
+            new_index = (alphabet[i] + 10) % 26
+            result += list(alphabet.keys())[new_index]
+    return result
