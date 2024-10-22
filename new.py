2ba5c762b1a0bb2f996c0ec0d2610900d5dbec00
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-22 22:03:19 +0300

    Next Solution

---
+++ new.py
@@ -0,0 +1,10 @@
+"""
+Find the most common letter (not space) in the string(always lowercase and 2 < words) and replace it with the letter.
+If such letters are two or more, choose the one that appears in the string( earlier.
+For example:
+('my mom loves me as never did', 't') => 'ty tot loves te as never did'
+('real talk bro', 'n') => 'neal talk bno'
+('great job go ahead', 'k') => 'grekt job go khekd'
+"""
+
+
