0fe890155111c8bd4d54b69ce9e5c7f03f0b3d87
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-10-29 21:11:30 +0300

    next_solution

--- new.py
+++ new.py
@@ -10,3 +10,51 @@ The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 
  The highest is 57.
 For C: do not mutate input.
 """
+
+
+def solve(s):
+    vowels = ("a", "e", "i", "o", "u")
+    alphabet = {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'd': 4,
+        'e': 5,
+        'f': 6,
+        'g': 7,
+        'h': 8,
+        'i': 9,
+        'j': 10,
+        'k': 11,
+        'l': 12,
+        'm': 13,
+        'n': 14,
+        'o': 15,
+        'p': 16,
+        'q': 17,
+        'r': 18,
+        's': 19,
+        't': 20,
+        'u': 21,
+        'v': 22,
+        'w': 23,
+        'x': 24,
+        'y': 25,
+        'z': 26,
+    }
+    max_value = 0
+    current_value = 0
+
+    for i in s:
+        if i in vowels:
+            # Гласная — сбрасываем текущую сумму, так как подстрока закончилась
+            max_value = max(max_value, current_value)
+            current_value = 0
+        else:
+            # Согласная — добавляем её значение
+            current_value += alphabet[i]
+
+    # Проверка на случай, если строка заканчивается согласной
+    max_value = max(max_value, current_value)
+
+    return max_value
