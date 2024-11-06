2038b8568577cde73d50a0bb6beb1f65b8805d49
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-11-06 20:00:33 +0300

    next_solution

--- new.py
+++ new.py
@@ -8,7 +8,25 @@ Example: The binary representation of 1234 is 10011010010, so the function shoul
 
 from collections import Counter
 
+import pytest
+
 
 def count_bits(n):
     binary = bin(n)[2:]
     return binary.count('1')
+
+
+@pytest.mark.parametrize("n, result", [
+    (0, 0),
+    (4, 1),
+    (7, 3),
+    (9, 2),
+    (10, 2),
+])
+def test_count_bits(n, result):
+    """Pytest"""
+    assert count_bits(n) == result
+
+
+if __name__ == '__main__':
+    pytest.main()
