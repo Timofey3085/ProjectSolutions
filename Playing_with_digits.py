b864a59a49ee48e72f8f33799f50d1e790e944c2
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-11-15 21:41:44 +0300

    next_solution

--- Playing_with_digits.py
+++ Playing_with_digits.py
@@ -16,11 +16,8 @@ Note: n and p will always be strictly positive integers.
 
 Examples:
 n = 89; p = 1 ---> 1 since 8¹ + 9² = 89 = 89 * 1
-
 n = 92; p = 1 ---> -1 since there is no k such that 9¹ + 2² equals 92 * k
-
 n = 695; p = 2 ---> 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
-
 n = 46288; p = 3 ---> 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 """
 import pytest
