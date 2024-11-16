69727d54ce95d189279008276d038a0ecdb1fdea
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-11-16 11:31:49 +0300

    next_solution

--- Playing_with_digits.py
+++ Playing_with_digits.py
@@ -1,16 +1,12 @@
 """
 Some numbers have funny properties. For example:
-
 89 --> 8¹ + 9² = 89 * 1
 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 Given two positive integers n and p, we want to find a positive integer k,
 if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
-
 In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :
-
 (a ** p + b ** p + 1 + c ** p + 2 + d ** p + 3 + ... ) = n ∗ k
-
 If it is the case we will return k, if not return -1.
 Note: n and p will always be strictly positive integers.
 
