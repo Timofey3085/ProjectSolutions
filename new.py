198546461636c83cb25b5c4ef0f4421128409160
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-11-18 22:43:11 +0300

    Next_Solution

--- new.py
+++ new.py
@@ -22,6 +22,7 @@ def disemvowel(string_):
     ("This website is for losers LOL!", "Ths wbst s fr lsrs LL!"),
     ("No offense but,\nYour writing is among the worst I've ever read", "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"),
     ("What are you, a communist?", "Wht r y,  cmmnst?"),
+    ("aeiou", ""),
 ])
 def test_disemvowel(string_, result):
     """Pytest"""
