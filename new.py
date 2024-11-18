d8574646058dd8a6b48ad803e330c3873ed5614f
Тимофей Разборщиков <ash_tray85@mail.ru>
2024-11-18 22:41:40 +0300

    Next_Solution

--- new.py
+++ new.py
@@ -20,6 +20,8 @@ def disemvowel(string_):
 
 @pytest.mark.parametrize("string_, result", [
     ("This website is for losers LOL!", "Ths wbst s fr lsrs LL!"),
+    ("No offense but,\nYour writing is among the worst I've ever read", "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"),
+    ("What are you, a communist?", "Wht r y,  cmmnst?"),
 ])
 def test_disemvowel(string_, result):
     """Pytest"""
