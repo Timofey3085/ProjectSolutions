ac228b775927312315bfcc69b66097cd91bb8b46
Тимофей Разборщиков <ash_tray85@mail.ru>
2025-01-30 22:17:27 +0300

    Next_Solution

--- Sorted_Union.py
+++ Sorted_Union.py
@@ -23,7 +23,7 @@ def unite_unique(*args):
 
 @pytest.mark.parametrize("args, result", [
     (([1, 2], [3, 4]), [1, 2, 3, 4]),
-    (([1, 3, 2], [5, 2, 1, 4], [2, 1]), [1, 3, 2, 5, 4])
+    (([1, 3, 2], [5, 2, 1, 4], [2, 1]), [1, 3, 2, 5, 4]),
 ])
 def test_unite_unique(args, result):
     """Pytest"""
